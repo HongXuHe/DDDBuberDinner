9ab8da9086ffdeeddc4164dea98f099a904f9278
HongXuHe <matt917917@gmail.com>
2023-11-03 21:34:47 +1100

    problemDetailsFactory custom

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,30 +1,36 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Api.Filters;
 using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 {
 builder.Services.AddApplication().AddInfrastructure(configuration:builder.Configuration);
-builder.Services.AddControllers(o =>
-{
-    o.Filters.Add<ErrorHandlingFilterAttribute>();
-});
+// builder.Services.AddControllers(o =>
+// {
+//     o.Filters.Add<ErrorHandlingFilterAttribute>();
+// });
+    builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 var app = builder.Build();
 {
+   // app.UseExceptionHandler("/error");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 //app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
