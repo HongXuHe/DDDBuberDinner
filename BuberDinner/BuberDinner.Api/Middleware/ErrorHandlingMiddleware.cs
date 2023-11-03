25c503ee62f3f51d58a779c400d022c71ea925bb
HongXuHe <matt917917@gmail.com>
2023-11-03 19:22:52 +1100

    use errorhandling middleware

---
+++ BuberDinner/BuberDinner.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace BuberDinner.Api.Middleware;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var code = HttpStatusCode.InternalServerError;
+        var result = JsonConvert.SerializeObject(new { error = exception.Message });
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+         await context.Response.WriteAsync(result);
+    }
+}
--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,3 +1,4 @@
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 using Microsoft.AspNetCore.Authentication;
@@ -21,6 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 
  app.UseAuthorization();
