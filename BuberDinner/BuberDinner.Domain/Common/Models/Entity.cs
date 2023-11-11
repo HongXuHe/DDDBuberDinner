ad04bf79ff49b736c151f8e33cde4f640ac081f7
HongXuHe <matt917917@gmail.com>
2023-11-11 13:10:11 +1100

    Create AggregateRoot and baseEntity and valueObject

---
+++ BuberDinner/BuberDinner.Domain/Common/Models/Entity.cs
@@ -0,0 +1,45 @@
+﻿namespace BuberDinner.Domain.Common.Models;
+
+public abstract class Entity<TId>:IEquatable<TId>
+where TId: notnull
+{
+    public TId Id { get; set; }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+
+    public bool Equals(TId? other)
+    {
+        return Equals((object?)other);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Entity<TId>)obj);
+    }
+
+    public static bool operator ==(Entity<TId> left, Entity<TId> right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Entity<TId> left, Entity<TId> right)
+    {
+        return !Equals(left, right);
+    }
+
+    protected bool Equals(Entity<TId> other)
+    {
+        return EqualityComparer<TId>.Default.Equals(Id, other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
